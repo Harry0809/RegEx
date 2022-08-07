6cf6b20df66e5f4be20ae989107fb5b4c671619c
Harish mahur <hmahur20@gmail.com>
2022-08-07 21:55:03 +0530

    modified:validating last name

--- RegexPattern.java
+++ RegexPattern.java
@@ -8,15 +8,14 @@ public class RegexPattern {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the First name : ");
-		String firstName = sc.nextLine();
+		System.out.println("Enter the Last name : ");
+		String lastName = sc.nextLine();
 
-		boolean validatefirstName = Pattern.matches("^[A-Z]{1}[a-z]{2,20}$", firstName);
-
-		if (validatefirstName) {
-			System.out.println("firstname is correct ");
+		boolean validatelastName = Pattern.matches("^[A-Z]{1}[a-z]{2,20}$", lastName);
+		if (validatelastName) {
+			System.out.println("lastname is correct ");
 		} else {
-			System.out.println("firstname is incorrect ");
+			System.out.println("lastname is incorrect ");
 		}
 	}
 
