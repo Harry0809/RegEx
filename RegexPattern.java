fffcb486da4c9ff36ea1fdbf9ac85b1b88214a46
Harish mahur <hmahur20@gmail.com>
2022-08-07 21:50:54 +0530

    modified:validating first name

--- RegexPattern.java
+++ RegexPattern.java
@@ -1,10 +1,23 @@
 package com.RegEx;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class RegexPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First name : ");
+		String firstName = sc.nextLine();
+
+		boolean validatefirstName = Pattern.matches("^[A-Z]{1}[a-z]{2,20}$", firstName);
+
+		if (validatefirstName) {
+			System.out.println("firstname is correct ");
+		} else {
+			System.out.println("firstname is incorrect ");
+		}
 	}
 
 }
